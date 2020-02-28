d31d0ba7badd203dd8553e2c82b30c11ba8b9970
jesse.luangaphayvong@my.simpson.edu <jesse.luangaphayvong@my.simpson.edu>
2020-02-27 18:35:31 -0600

    Work on Thread

--- ThreadUnity/Assets/PlayerController.cs
+++ ThreadUnity/Assets/PlayerController.cs
@@ -4,23 +4,145 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rb2D;
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rb2D;
+    private Collider2D coll;
+    private Animator anim;
+    private float distToGround;
+    private bool moving;
+
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private int CharacterSpeed = 6;
+    [SerializeField] private float hurtForce = 6f;
+    [SerializeField] private float jumpForce = 15f;
+
+    private enum State { idle, running, jumping, attacking, hurt, falling}
+
+    private State state = State.idle;
+
+    private void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    private void Movement()
+    {
+        float hDirection = Input.GetAxis("Horizontal");
+
+        if (hDirection < 0)
+        {
+           rb2D.velocity = new Vector2(-CharacterSpeed, rb2D.velocity.y);
+           transform.localScale = new Vector2(-1, 1);
+        }
+        else if (hDirection > 0)
+        {
+            rb2D.velocity = new Vector2(CharacterSpeed, rb2D.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+        }
+        else
+        {
+            rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+        }
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, jumpForce);
+            state = State.jumping;
+        }
+        velocityState();
+        anim.SetInteger("state", (int)state);
+    }
+
+    private void velocityState()
+    {
+
+        if(state == State.jumping)
+        {
+            if(rb2D.velocity.y < 0.1f)
+            {
+                state = State.falling;
+            }
+        }
+        else if(state == State.falling)
+        {
+            if(coll.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }
+        else if(Mathf.Abs(rb2D.velocity.x) > 1f)
+        {
+            state = State.running;
+        }
+        else
+        {
+            state = State.idle;
+        }
+    }
+
+    private void AnimationState()
+    {
+        if (state == State.jumping)
+        {
+            if (rb2D.velocity.y < 0.1f)
+            {
+                state = State.falling;
+            }
+        }
+        else if (state == State.falling)
+        {
+            if (coll.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }
+        else if (state == State.hurt)
+        {
+            if (Mathf.Abs(rb2D.velocity.x) < .1f)
+            {
+                state = State.idle;
+            }
+        }
+        else if (Mathf.Abs(rb2D.velocity.x) > 2f)
+        {
+            state = State.running;
+        }
+        else
+        {
+            state = State.idle;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            if (state == State.attacking)
+            {
+                Destroy(other.gameObject);
+            }
+            else
+            {
+                state = State.hurt;
+                if(other.gameObject.transform.position.x > transform.position.x)
+                {
+                    rb2D.velocity = new Vector2(-hurtForce, rb2D.velocity.y);
+                }
+                else
+                {
+                    rb2D.velocity = new Vector2(hurtForce, rb2D.velocity.y);
+                }
+            }
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
-        float vertical = Input.GetAxis("Vertical");
-        Debug.Log(vertical);
-        Vector2 position = rb2D.position;
-        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 5.0f * vertical * Time.deltaTime;
-        rb2D.position = position;
+        if (state != State.hurt)
+        {
+            Movement();
+        }
+        AnimationState();
     }
 }
